06815c98aa0346c039902f90a6c3611f941a4595
宏希 有島 <c5bw9sqg9t@privaterelay.appleid.com>
2022-08-15 20:13:57 +0900

    新規登録バリデーションエラーの表示

--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -14,9 +14,6 @@
 </head>
 <body>
   <form action="{{ route('registerPost') }}" method="POST">
-    @foreach ($errors->all() as $error)
-      <li>{{$error}}</li>
-    @endforeach
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
       <div class="w-25 vh-75 border p-3">
         <div class="register_form">
@@ -26,12 +23,18 @@
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name" name="over_name">
               </div>
+                @if ($errors->has('over_name'))
+                 <li>{{$errors->first('over_name')}}</li>
+                @endif
             </div>
             <div class="" style="width:140px">
               <label class=" d-block m-0" style="font-size:13px">名</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name" name="under_name">
               </div>
+               @if ($errors->has('under_name'))
+                 <li>{{$errors->first('under_name')}}</li>
+               @endif
             </div>
           </div>
           <div class="d-flex mt-3" style="justify-content:space-between">
@@ -40,12 +43,18 @@
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name_kana" name="over_name_kana">
               </div>
-            </div>
+                @if ($errors->has('over_name_kana'))
+                 <li>{{$errors->first('over_name_kana')}}</li>
+                @endif
+             </div>
             <div class="" style="width:140px">
               <label class="d-block m-0" style="font-size:13px">メイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name_kana" name="under_name_kana">
               </div>
+                @if ($errors->has('under_name_kana'))
+                 <li>{{$errors->first('under_name_kana')}}</li>
+                @endif
             </div>
           </div>
           <div class="mt-3">
@@ -53,6 +62,9 @@
             <div class="border-bottom border-primary">
               <input type="mail" class="w-100 border-0 mail_address" name="mail_address">
             </div>
+               @if ($errors->has('mail_address'))
+                 <li>{{$errors->first('mail_address')}}</li>
+               @endif
           </div>
         </div>
         <div class="mt-3">
@@ -62,6 +74,9 @@
           <label style="font-size:13px">女性</label>
           <input type="radio" name="sex" class="sex" value="3">
           <label style="font-size:13px">その他</label>
+               @if ($errors->has('sex'))
+                 <li>{{$errors->first('sex')}}</li>
+               @endif
         </div>
         <div class="mt-3">
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
@@ -145,7 +160,10 @@
             <option value="30">30</option>
             <option value="31">31</option>
           </select>
-          <label style="font-size:13px">月</label>
+          <label style="font-size:13px">日</label>
+            @if ($errors->has('old_year'))
+              <li>{{$errors->first('old_year')}}</li>
+            @endif
         </div>
         <div class="mt-3">
           <label class="d-block m-0" style="font-size:13px">役職</label>
@@ -157,6 +175,9 @@
           <label style="font-size:13px">教師(英語)</label>
           <input type="radio" name="role" class="other_role role" value="4">
           <label style="font-size:13px" class="other_role">生徒</label>
+              @if ($errors->has('role'))
+                 <li>{{$errors->first('role')}}</li>
+              @endif
         </div>
         <div class="select_teacher d-none">
           <label class="d-block m-0" style="font-size:13px">選択科目</label>
@@ -172,12 +193,18 @@
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password" name="password">
           </div>
+             @if ($errors->has('password'))
+                 <li>{{$errors->first('password')}}</li>
+             @endif
         </div>
         <div class="mt-3">
           <label class="d-block m-0" style="font-size:13px">確認用パスワード</label>
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password_confirmation" name="password_confirm">
           </div>
+              @if ($errors->has('password_confirm'))
+                <li>{{$errors->first('password_confirm')}}</li>
+              @endif
         </div>
         <div class="mt-5 text-right">
           <input type="submit" class="btn btn-primary register_btn" disabled value="新規登録" onclick="return confirm('登録してよろしいですか？')">
